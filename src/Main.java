1f0572941ec6411db64b43c101ad0cdfda9d36c9
Mohammed-khair <mrdk3002@gmail.com>
2023-02-06 23:31:38 +0300

    removed errors

--- src/Main.java
+++ src/Main.java
@@ -12,18 +12,5 @@ public class Main {
         serverThread.start();
         hostThread.start();
         clientThread.start();
-
-        byte data[] = new byte[100];
-        data[0] = 0;
-        data[1] = 1;
-        String filename = "test.txt";
-        data[2] = filename.getBytes(); //filename
-        String mode = "octet";
-        data[3] = mode.getBytes();
-        data[4] = 0;
-
-
-
-
     }
 }
